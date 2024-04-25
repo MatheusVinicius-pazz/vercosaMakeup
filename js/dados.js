93cca645b2eb6d3b38c288fffd5b62ba153bcda5
MatheusVinicius-pazz <mvpr1@aluno.ifal.edu.br>
2024-04-24 22:44:59 -0300

    new

--- js/dados.js
+++ js/dados.js
@@ -11,93 +11,101 @@ var MENU = {
         },
         {
             "id": "shake-shack-shackburger-8-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
+            "img": "./img/cardapio/burguers/cola-cilios.jpeg",
             "name": "Cola para cílios postiços incolor Macrilan",
             "dsc": "Cola para cílios postiços incolor Macrilan",
             "price": 17.5
         },
         {
             "id": "gotts-cheeseburger-kit-for-4",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
+            "img": "./img/cardapio/burguers/po-facial-banana.jpeg",
             "name": "Pó Facial Translúcido Banana ",
             "dsc": "Banana Star Glow Phállebeauty",
             "price": 14.5
         },
         {
             "id": "le-big-matt-kit-for-6",
-            "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
+            "img": "./img/cardapio/burguers/paleta-de-sombra.jpeg",
             "name": "Paleta de Sombra",
             "dsc": "Nascente Areia Jasmyne",
             "price": 15.5
         },
         {
             "id": "shake-shack-shackburger-16-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
+            "img": "./img/cardapio/burguers/cilios-posticos.jpeg",
             "name": "Cílios Postiços Mey Beauty",
             "dsc": "Vem também um kit Escovinha + tesourinha, que vai facilitar ainda mas na hora de usar seu cílios postiços.",
             "price": 89
         },
         {
             "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
-            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
+            "img": "./img/cardapio/burguers/sabonete-make.jpeg",
             "name": "Sabonete new make out",
             "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
             "price": 15
         },
         {
             "id": "double-stack-burger-kit-for-4",
-            "img": "./img/cardapio/burguers/handf-double-stack-burger-kit-for-4.4ee9f54b1d6087e9996335f07c13e5cd.jpg",
+            "img": "./img/cardapio/burguers/gel-rosa.jpeg",
             "name": "Gel rosa mosqueta dermachem",
             "dsc": "Gel rosa mosqueta dermachem",
             "price": 13
         },
         {
             "id": "goldbelly-burger-bash-pack",
-            "img": "./img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
+            "img": "./img/cardapio/burguers/gel-verde.jpeg",
             "name": "Gel verde antioleosidade",
             "dsc": "Gel verde antioleosidade",
             "price": 13
         },
-        {
-            "id": "burger-au-poivre-kit-4-pack",
-            "img": "./img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
-            "name": "Kit de pincel",
-            "dsc": "Kit de pincel",
-            "price": 25.5
-        },
+      
+        
+       
+    ],
+    "pizzas": [
         {
             "id": "goldbelly-burger-blend-4-lbs",
-            "img": "./img/cardapio/burguers/goldbelly-burger-blend-1-lb.13a21b66edf7173a59c75c3a6d2f981b.jpg",
+            "img": "./img/cardapio/burguers/blush-tradicional.jpeg",
             "name": "Blush tradicional max love",
             "dsc": "Blush tradicional max love",
             "price": 14.5
         },
         {
             "id": "gotts-complete-cheeseburger-kit-for-8",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-8.092aa049d00286fa1733d720decc782e.jpg",
+            "img": "./img/cardapio/burguers/esponja-gota.jpeg",
             "name": "Espoja gota make",
             "dsc": "Espoja gota make",
             "price": 10
         },
         {
             "id": "gramercy-tavern-burger-kielbasa-combo",
-            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
+            "img": "./img/cardapio/burguers/lip-oil.jpeg",
             "name": "Lip oil lua e neve",
             "dsc": "Lip oil lua e neve",
             "price": 13.5
         },
-        
-       
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/kit-pincel.jpeg",
+            "name": "Kit de pincel",
+            "dsc": "Kit de pincel",
+            "price": 25.5
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/esfoliante-tamaras.jpeg",
+            "name": "Esfoliante Corporal Tamaras Maduras",
+            "dsc": "Esfoliante Corporal Tamaras Maduras",
+            "price": 18.5
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/esfoliante-bumbum.jpeg",
+            "name": "Esfoliante Bum Bum lisinho",
+            "dsc": "Kit de pincel",
+            "price": 25.5
+        }
     ]
     
 }
 
-var novoProduto =  {
-    "id": "gramercy-tavern-burger-kielbasa-combo",
-    "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
-    "name": "...",
-    "dsc": "Lip oil lua e neve",
-    "price": 13.5
-}
-
-MENU.burgers.push(novoProduto);
